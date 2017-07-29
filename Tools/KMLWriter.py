fee0b14b5f136109b219607407a27be08e266d94
Nick Franklin <n.o.franklin@gmail.com>
2017-07-29 19:33:35 -0400

    Improvements to KMLWriter

--- Tools/KMLWriter.py
+++ Tools/KMLWriter.py
@@ -19,10 +19,17 @@ class KMLWriter(object):
         return simplekml.Color.changealphaint(num, color)
 
     def generate(self, df, column_names, color=simplekml.Color.cornflowerblue,
+                 schema_types={},
                  map_name='Map', filename='~/Downloads/KMLWriter.kml'):
         """Generate KML from DataFrame."""
 
         kml = simplekml.Kml(name=map_name)
+
+        if schema_types:
+            schema = kml.newschema()
+            for name, typ in schema_types.items():
+                schema.newsimplefield(name=name, type=typ)
+
         for row in df.iterrows():
             if 'shape_name_column' in column_names:
                 name = row[1][column_names['shape_name_column']].encode('ascii', 'ignore')
@@ -38,20 +45,22 @@ class KMLWriter(object):
                 color = self.get_color_scale(color, 0.5)
 
             if isinstance(row[1].geometry, shapely.geometry.polygon.Polygon):
-                poly = kml.newpolygon(name=name, description=description)
-                poly.outerboundaryis = row[1].geometry.exterior.coords
-                poly.style.polystyle.color = color
-                poly.style.polystyle.fill = 1
-                poly.style.polystyle.outline = 1
-
+                obj = kml.newpolygon(name=name, description=description)
+                obj.outerboundaryis = row[1].geometry.exterior.coords
             elif isinstance(row[1].geometry, shapely.geometry.multipolygon.MultiPolygon):
-                multipnt = kml.newmultigeometry(name=name, description=description)
+                obj = kml.newmultigeometry(name=name, description=description)
                 for geom in row[1].geometry.geoms:
-                    poly = multipnt.newpolygon()
+                    poly = obj.newpolygon()
                     poly.outerboundaryis = geom.exterior.coords
-                multipnt.style.polystyle.color = color
-                multipnt.style.polystyle.fill = 1
-                multipnt.style.polystyle.outline = 1
+
+            obj.style.polystyle.color = color
+            obj.style.polystyle.fill = 1
+            obj.style.polystyle.outline = 0
+
+            if schema_types:
+                obj.extendeddata.schemadata.schemaurl = schema.id
+                for name in schema_types:
+                    obj.extendeddata.schemadata.newsimpledata(name, row[1][name])
 
         kml.save(path=filename)
         print('Saved {}'.format(filename))
