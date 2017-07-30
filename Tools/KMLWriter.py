f8e9b991801575c708479981f1f9f4fbb74ffef8
Nick Franklin <n.o.franklin@gmail.com>
2017-07-29 20:20:14 -0400

    Extend KMLWriter to use matplotlib colormaps

--- Tools/KMLWriter.py
+++ Tools/KMLWriter.py
@@ -6,61 +6,83 @@
 
 import simplekml
 import shapely
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
-class KMLWriter(object):
-    """Convert a Geopandas dataframe of Polygons/MultiPolygons to KML for use in Google Maps."""
 
-    def get_color_scale(self, color, scale):
-        """Generate a color from a scale in the range 0..1 inclusive."""
-        try:
-            num = int(255 * (scale**2))
-        except ValueError:
-            num = 0
-        return simplekml.Color.changealphaint(num, color)
+def get_color_scale(color, scale):
+    """Generate a color from a scale in the range 0..1 inclusive."""
+    try:
+        num = int(255 * (scale**2))
+    except ValueError:
+        num = 0
+    return simplekml.Color.changealphaint(num, color)
 
-    def generate(self, df, column_names, color=simplekml.Color.cornflowerblue,
-                 schema_types={},
-                 map_name='Map', filename='~/Downloads/KMLWriter.kml'):
-        """Generate KML from DataFrame."""
+def get_color_range(colormap, scale):
+    """Get color from matplotlib colormap.  See https://matplotlib.org/users/colormaps.html."""
+    cmap = plt.cm.get_cmap(colormap)
+    try:
+        num = int(cmap.N * scale)
+    except ValueError:
+        num = 0
+    rgb = cmap(num)[:3]
+    hexstring = colors.rgb2hex(rgb)
+    hex_color = simplekml.Color.hex(hexstring[1:])
+    return simplekml.Color.changealphaint(150, hex_color)
 
-        kml = simplekml.Kml(name=map_name)
+def generate(df, column_names, color_detail={},
+             schema_types={},
+             map_name='Map', filename='~/Downloads/KMLWriter.kml'):
+    """Generate KML from DataFrame."""
+
+    kml = simplekml.Kml(name=map_name)
+
+    if schema_types:
+        schema = kml.newschema()
+        for name, typ in schema_types.items():
+            schema.newsimplefield(name=name, type=typ)
+
+    for row in df.iterrows():
+        if 'shape_name_column' in column_names:
+            name = row[1][
+                column_names['shape_name_column']].encode(
+                'ascii', 'ignore')
+        else:
+            name = None
+        if 'description_column' in column_names:
+            description = row[1][
+                column_names['description_column']].encode(
+                'ascii', 'ignore')
+        else:
+            description = None
+
+        if isinstance(row[1].geometry, shapely.geometry.polygon.Polygon):
+            obj = kml.newpolygon(name=name, description=description)
+            obj.outerboundaryis = row[1].geometry.exterior.coords
+        elif isinstance(row[1].geometry, shapely.geometry.multipolygon.MultiPolygon):
+            obj = kml.newmultigeometry(name=name, description=description)
+            for geom in row[1].geometry.geoms:
+                poly = obj.newpolygon()
+                poly.outerboundaryis = geom.exterior.coords
+
+        if ('color' in color_detail) and ('color_scale' in color_detail):
+            obj.style.polystyle.color = get_color_scale(
+                color_detail['color'], row[1][color_detail['color_scale']])
+        elif ('colormap' in color_detail) and ('color_scale' in color_detail):
+            obj.style.polystyle.color = get_color_range(
+                color_detail['colormap'], row[1][color_detail['color_scale']])
+        else:
+            obj.style.polystyle.color = get_color_scale(
+                simplekml.Color.cornflowerblue, 0.5)
+
+        obj.style.polystyle.fill = 1
+        obj.style.polystyle.outline = 0
 
         if schema_types:
-            schema = kml.newschema()
-            for name, typ in schema_types.items():
-                schema.newsimplefield(name=name, type=typ)
+            obj.extendeddata.schemadata.schemaurl = schema.id
+            for name in schema_types:
+                obj.extendeddata.schemadata.newsimpledata(
+                    name, row[1][name])
 
-        for row in df.iterrows():
-            if 'shape_name_column' in column_names:
-                name = row[1][column_names['shape_name_column']].encode('ascii', 'ignore')
-            else:
-                name = None
-            if 'description_column' in column_names:
-                description = row[1][column_names['description_column']].encode('ascii', 'ignore')
-            else:
-                description = None
-            if 'scale_column' in column_names:
-                color = self.get_color_scale(color, row[1][column_names['scale_column']])
-            else:
-                color = self.get_color_scale(color, 0.5)
-
-            if isinstance(row[1].geometry, shapely.geometry.polygon.Polygon):
-                obj = kml.newpolygon(name=name, description=description)
-                obj.outerboundaryis = row[1].geometry.exterior.coords
-            elif isinstance(row[1].geometry, shapely.geometry.multipolygon.MultiPolygon):
-                obj = kml.newmultigeometry(name=name, description=description)
-                for geom in row[1].geometry.geoms:
-                    poly = obj.newpolygon()
-                    poly.outerboundaryis = geom.exterior.coords
-
-            obj.style.polystyle.color = color
-            obj.style.polystyle.fill = 1
-            obj.style.polystyle.outline = 0
-
-            if schema_types:
-                obj.extendeddata.schemadata.schemaurl = schema.id
-                for name in schema_types:
-                    obj.extendeddata.schemadata.newsimpledata(name, row[1][name])
-
-        kml.save(path=filename)
-        print('Saved {}'.format(filename))
+    kml.save(path=filename)
+    print('Saved {}'.format(filename))
