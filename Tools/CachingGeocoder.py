26eb0f3a063b4b1b902a9cf727618914770df571
Nick Franklin <n.o.franklin@gmail.com>
2017-10-28 14:22:05 -0400

    Better error handling for CachingGeocoder

--- Tools/CachingGeocoder.py
+++ Tools/CachingGeocoder.py
@@ -16,6 +16,8 @@ A very simple, append-only cache is implemented in a CSV on the local disk.
 import sys
 import googlemaps
 import json
+import pandas as pd
+import geopandas as gpd
 from tqdm import tqdm
 from shapely.geometry import Point
 from shapely.geometry import mapping, shape
@@ -23,15 +25,19 @@ from shapely.geometry import mapping, shape
 
 def create_geodf(df, api_key):
     # Populate cache
-    geolog = pd.read_csv('geolocation_log.csv',
-                         header=None, error_bad_lines=False,
-                         names=['address_string', 'point_json'], quotechar="'")
-    geolog.drop_duplicates(inplace=True)
-    cache = {}
-    for index, row in geolog.iterrows():
-        cache[row.address_string] = shape(json.loads(row.point_json))
+    try:
+        geolog = pd.read_csv('geolocation_log.csv',
+                             header=None, error_bad_lines=False,
+                             names=['address_string', 'point_json'], quotechar="'")
+        geolog.drop_duplicates(inplace=True)
+        cache = {}
+        for index, row in geolog.iterrows():
+            cache[row.address_string] = shape(json.loads(row.point_json))
 
-    print "Loaded " + str(len(cache)) + " into cache"
+        print "Loaded " + str(len(cache)) + " into cache"
+    except IOError:
+        cache = {}
+        print "No log found, continuing with empty cache"
     sys.stdout.flush()
     sys.stderr.flush()
 
@@ -42,18 +48,22 @@ def create_geodf(df, api_key):
     geometry_col = []
     for index, row in tqdm(df.iterrows(), total=len(df)):
         point = None
-        if row.address_string in cache:
-            point = cache[row.address_string]
-            cache_hits += 1
-        else:
-            geocode_result = gmaps.geocode(row.address_string)
-            cache_misses += 1
-            if geocode_result:
-                point = Point(geocode_result[0]['geometry']['location']['lng'],
-                              geocode_result[0]['geometry']['location']['lat'])
-                log.write("".join(["'", row.address_string,
-                                   "','", json.dumps(mapping(point)), "'\n"]))
-                cache[row.address_string] = point
+        if row.address_string:
+            if row.address_string in cache:
+                point = cache[row.address_string]
+                cache_hits += 1
+            else:
+                geocode_result = gmaps.geocode(row.address_string)
+                cache_misses += 1
+                if geocode_result:
+                    point = Point(geocode_result[0]['geometry']['location']['lng'],
+                                  geocode_result[0]['geometry']['location']['lat'])
+                    try:
+                        log.write("".join(["'", row.address_string,
+                                       "','", json.dumps(mapping(point)), "'\n"]))
+                    except TypeError:
+                        print "Error writing to log, continuing"
+                    cache[row.address_string] = point
         geometry_col.append(point)
     log.close()
     sys.stdout.flush()
